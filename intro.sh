c3d8164014589458f0088955eb9e90793c6e04f3
Michael J. Henry <mjhenry.cpts224@gmail.com>
2017-08-30 20:05:43 -0700

    Introductory code for Michael

---
+++ intro.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello, let me introduce myself"
+
+NAME=Michael
+HOMETOWN=Indianapolis
+MAJOR=CS
+FAVORITE_HOBBY=read
+
+echo "My name is $NAME. I am originally from $HOMETOWN. My major is $MAJOR and in my free time I like to $FAVORITE_HOBBY"
