ac40b0fcadfe5ce6e4deed59e27fc99dd0f02fb1
Rupali Rajput <rupali.rajput@wsu.edu>
2017-09-06 02:12:04 -0700

    HW1 intro.sh Modified by Rupali Rajput.

--- intro.sh
+++ intro.sh
@@ -2,9 +2,9 @@
 
 echo "Hello, let me introduce myself"
 
-NAME=Michael
-HOMETOWN=Indianapolis
+NAME=Rupali
+HOMETOWN=Mumbai,\ India
 MAJOR=CS
-FAVORITE_HOBBY=read
+FAVORITE_HOBBY=cook
 
 echo "My name is $NAME. I am originally from $HOMETOWN. My major is $MAJOR and in my free time I like to $FAVORITE_HOBBY"
